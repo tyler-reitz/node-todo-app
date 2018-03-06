5aa90de41d961c3d9b97a99345db20652444ab89
Tyler Reitz <tdreitz@gmail.com>
2018-03-05 20:52:41 -0800

    fix mongodb_uri

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 mongoose.connect(
-  process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
   { useMongoClient: true }
 )
 

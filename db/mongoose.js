19cfd908642a8d5071653f3353e028df340b56af
Tyler Reitz <tdreitz@gmail.com>
2018-03-05 20:47:11 -0800

    setup heroku env

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true })
+mongoose.connect(
+  process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp',
+  { useMongoClient: true }
+)
 
 module.exports = {
   mongoose

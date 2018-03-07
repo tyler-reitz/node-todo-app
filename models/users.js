54fe7743a17ac9907342f1318089669cdffb2587
Tyler Reitz <tdreitz@gmail.com>
2018-03-06 19:14:36 -0800

    refactor env setup

--- models/users.js
+++ models/users.js
@@ -80,7 +80,7 @@ UserSchema.statics.findByToken = function(token) {
   let decoded
 
   try {
-    decoded = jwt.verify(token, "abc123")
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
   } catch (e) {
     return Promise.reject()
   }
@@ -103,7 +103,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   const user = this
   const access = 'auth'
-  const token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString()
+  const token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString()
 
   user.tokens = user.tokens.concat([{ access, token }])
   
